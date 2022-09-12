14c4d2fac7774068b01118f060403b77d3a612d5
Shahrear Ahamed <its.shahrear@gmail.com>
2022-09-12 13:49:58 +0600

    add register function

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -1,7 +1,48 @@
-import { apiSlice } from "../api/apiSlice";
+import {apiSlice} from "../api/apiSlice";
+import {userLoggedIn} from "./authSlice";
 
 export const authApi = apiSlice.injectEndpoints({
-    endpoints: (builder) => {
-        // endpoints here
-    },
+    endpoints: (builder) => ({
+        register: builder.mutation({
+            query: (data) => ({
+                url: "/register",
+                method: "POST",
+                body: data
+            }),
+            async onQueryStarted(arg, {queryFulfilled, dispatch}) {
+                try {
+                    const result = await queryFulfilled;
+                    const userCredential = {
+                        accessToken: result.data.accessToken,
+                        user: result.data.user
+                    }
+                    localStorage.setItem("Auth", JSON.stringify(userCredential))
+                    dispatch(userLoggedIn(userCredential))
+                } catch (e) {
+
+                }
+            }
+        }),
+        login: builder.mutation({
+            query: (data) => ({
+                url: "/login",
+                method: "POST",
+                body: data
+            }),
+            async onQueryStarted(arg, {queryFulfilled, dispatch}) {
+                try {
+                    const result = await queryFulfilled;
+                    const userCredential = {
+                        accessToken: result.data.accessToken,
+                        user: result.data.user
+                    }
+                    localStorage.setItem("Auth", JSON.stringify(userCredential))
+                    dispatch(userLoggedIn(userCredential))
+                } catch (e) {
+
+                }
+            }
+        }),
+    }),
 });
+export const {useRegisterMutation, useLoginMutation} = authApi;

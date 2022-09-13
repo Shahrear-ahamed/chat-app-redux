4f6cff237ee48c0074a6e14d83242c76d2549549
Shahrear Ahamed <its.shahrear@gmail.com>
2022-09-13 19:33:02 +0600

    add user name and avater in message section

--- src/components/inbox/chatbody/ChatBody.js
+++ src/components/inbox/chatbody/ChatBody.js
@@ -1,9 +1,7 @@
-// import Blank from "./Blank";
 import ChatHead from "./ChatHead";
 import Messages from "./Messages";
 import Options from "./Options";
 import {useParams} from "react-router-dom";
-import {useSelector} from "react-redux";
 import {useGetMessagesQuery} from "../../../features/messages/messagesApi";
 import Error from "../../ui/Error";
 
@@ -22,10 +20,7 @@ export default function ChatBody() {
         content = <div>No messages found</div>
     } else if (!isLoading && !isError && messages?.length > 0) {
         content = <>
-            <ChatHead
-                avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
-                name="Akash Ahmed"
-            />
+            <ChatHead message={messages[0]}/>
             <Messages messages={messages}/>
             <Options/>
         </>
@@ -34,7 +29,6 @@ export default function ChatBody() {
     return (<div className="w-full lg:col-span-2 lg:block">
         <div className="w-full grid conversation-row-grid">
             {content}
-            {/* <Blank /> */}
         </div>
     </div>);
 }
